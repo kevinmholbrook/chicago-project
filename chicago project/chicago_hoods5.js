018ac7b6a283a418a88363cc6709fc439e05cf81
unknown <jazzstephens2015@gmail.com>
2019-06-16 17:16:22 -0500

    added google maps interface to enable overlaying of metra/cta train lines. Also updated the html file to add in leaflet plugin Leaflet GridLayer Google Mutant, which allows you to use google maps with leaflet

--- chicago project/chicago_hoods5.js
+++ chicago project/chicago_hoods5.js
@@ -1,7 +1,16 @@
 // Set link
 var link = "https://data.cityofchicago.org/resource/igwz-8jzy.geojson"
 
-
+var ghostIcon = L.icon({
+  iconUrl: "Images/ghost-icon5.png",
+  iconSize: [35, 35],
+  iconAnchor: [20, 20],
+});
+var CPLIcon = L.icon({
+  iconUrl: "Images/pin.png",
+  iconSize: [35, 35],
+  iconAnchor: [20, 20],
+});
 //Function that will determine the color of a Chicago neighborhood based on the area it belongs to
 function chooseColor(community) {
   switch (community) {
@@ -14,17 +23,17 @@ function chooseColor(community) {
       return "darkgreen";
     case "JEFFERSON PARK":
       return "darkgreen";
-    case "FOREST GLEN": 
+    case "FOREST GLEN":
       return "darkgreen";
-    case "NORTH PARK":  
+    case "NORTH PARK":
       return "darkgreen";
     case "ALBANY PARK":
       return "darkgreen";
-    case "WEST RIDGE":  
+    case "WEST RIDGE":
       return "darkgreen";
     case "LINCOLN SQUARE":
       return "darkgreen";
-    case "ROGERS PARK": 
+    case "ROGERS PARK":
       return "darkgreen";
     case "EDGEWATER":
       return "darkgreen";
@@ -39,7 +48,7 @@ function chooseColor(community) {
       return "cyan";
     case "LOGAN SQUARE":
       return "cyan";
-    case "NORTH CENTER": 
+    case "NORTH CENTER":
       return "cyan";
     // "Northwest Side"
     case "BELMONT CRAGIN":
@@ -50,7 +59,7 @@ function chooseColor(community) {
       return "darkslateblue";
     case "IRVING PARK":
       return "darkslateblue";
-    case "MONTCLARE": 
+    case "MONTCLARE":
       return "darkslateblue";
     case "PORTAGE PARK":
       return "darkslateblue";
@@ -63,7 +72,7 @@ function chooseColor(community) {
       return "red";
     case "WEST GARFIELD PARK":
       return "red";
-    case "EAST GARFIELD PARK": 
+    case "EAST GARFIELD PARK":
       return "red";
     case "NEAR WEST SIDE":
       return "red";
@@ -72,7 +81,7 @@ function chooseColor(community) {
     case "SOUTH LAWNDALE":
       return "red";
     case "LOWER WEST SIDE":
-      return "red"; 
+      return "red";
     // "Central"
     case "NEAR NORTH SIDE":
       return "fuchsia";
@@ -82,100 +91,100 @@ function chooseColor(community) {
       return "fuchsia";
     // "Southwest Side"
     case "GARFIELD RIDGE":
-        return "orange";
+      return "orange";
     case "CLEARING":
-        return "darkorange";
+      return "darkorange";
     case "ARCHER HEIGHTS":
-        return "darkorange";
+      return "darkorange";
     case "WEST ELSDON":
-        return "darkorange";
-    case "WEST LAWN": 
-        return "darkorange";
+      return "darkorange";
+    case "WEST LAWN":
+      return "darkorange";
     case "BRIGHTON PARK":
-        return "darkorange";
+      return "darkorange";
     case "GAGE PARK":
-        return "darkorange";
+      return "darkorange";
     case "CHICAGO LAWN":
-        return "darkorange";
+      return "darkorange";
     case "MCKINLEY PARK":
-        return "darkorange"; 
+      return "darkorange";
     case "NEW CITY":
-        return "darkorange";
+      return "darkorange";
     case "WEST ENGLEWOOD":
-        return "darkorange";
+      return "darkorange";
     case "ENGLEWOOD":
-        return "darkorange"; 
+      return "darkorange";
     // "South Side"
     case "BRIDGEPORT":
-        return "yellow";
+      return "yellow";
     case "ARMOUR SQUARE":
-        return "yellow";
+      return "yellow";
     case "DOUGLAS":
-        return "yellow";
+      return "yellow";
     case "OAKLAND":
-        return "yellow";
-    case "GRAND BOULEVARD": 
-        return "yellow";
+      return "yellow";
+    case "GRAND BOULEVARD":
+      return "yellow";
     case "FULLER PARK":
-        return "yellow";
+      return "yellow";
     case "WASHINGTON PARK":
-        return "yellow";
+      return "yellow";
     case "HYDE PARK":
-        return "yellow";
+      return "yellow";
     case "WOODLAWN":
-        return "yellow"; 
+      return "yellow";
     case "GREATER GRAND CROSSING":
-        return "yellow";
+      return "yellow";
     case "SOUTH SHORE":
-        return "yellow";
+      return "yellow";
     case "KENWOOD":
-        return "yellow"; 
+      return "yellow";
     // "Far Southwest Side"
     case "ASHBURN":
-        return "green";
+      return "green";
     case "AUBURN GRESHAM":
-        return "green";
+      return "green";
     case "BEVERLY":
-        return "green";
+      return "green";
     case "WASHINGTON HEIGHTS":
-        return "green";
-    case "MOUNT GREENWOOD": 
-        return "green";
+      return "green";
+    case "MOUNT GREENWOOD":
+      return "green";
     case "MORGAN PARK":
-        return "green";
+      return "green";
     // "Far Southwest Side"
     case "CHATHAM":
-        return "cadetblue";
+      return "cadetblue";
     case "AVALON PARK":
-        return "cadetblue";
+      return "cadetblue";
     case "SOUTH CHICAGO":
-        return "cadetblue";
+      return "cadetblue";
     case "CALUMET HEIGHTS":
-        return "cadetblue";
-    case "BURNSIDE": 
-        return "cadetblue";
+      return "cadetblue";
+    case "BURNSIDE":
+      return "cadetblue";
     case "ROSELAND":
-        return "cadetblue";
+      return "cadetblue";
     case "WEST PULLMAN":
-        return "cadetblue";
+      return "cadetblue";
     case "RIVERDALE":
-        return "cadetblue";
+      return "cadetblue";
     case "PULLMAN":
-        return "cadetblue"; 
+      return "cadetblue";
     case "SOUTH DEERING":
-        return "cadetblue";
+      return "cadetblue";
     case "EAST SIDE":
-        return "cadetblue";
+      return "cadetblue";
     case "HEGEWISCH":
-        return "cadetblue";
+      return "cadetblue";
 
-  default:
-    return "grey";
+    default:
+      return "grey";
   }
 }
 
 // Grabbing our GeoJSON data..
-d3.json(link, function(data) {
+d3.json(link, function (data) {
   //Test print
   console.log(data.features[1].properties.community);
   // Call createFeatures function defined below
@@ -183,11 +192,11 @@ d3.json(link, function(data) {
 });
 
 // Define createFeatures function
-function createFeatures (neighborhoodData) {
+function createFeatures(neighborhoodData) {
   // Define neighborhood layer
   var neighborhoods = L.geoJson(neighborhoodData, {
     // Style each feature (in this case a neighborhood)
-    style: function(feature) {
+    style: function (feature) {
       return {
         color: "white",
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
@@ -197,18 +206,18 @@ function createFeatures (neighborhoodData) {
       };
     },
     // Called on each feature
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       // Set mouse events to change map styling
       layer.on({
         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
+        mouseover: function (event) {
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.9
           });
         },
         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
+        mouseout: function (event) {
           layer = event.target;
           layer.setStyle({
             strokeweight: 2,
@@ -228,35 +237,45 @@ function createFeatures (neighborhoodData) {
   // Call createMap function defined below
   createMap(neighborhoods);
 };
-//ghost Chicago
+
 var hauntedMarkers = [];
-d3.csv("haunted.csv", function(error, hauntedData) {
-  
+d3.csv("haunted.csv", function (error, hauntedData) {
+
   if (error) return console.warn(error);
 
   // Cast each hours value in tvData as a number using the unary + operator
-  hauntedData.forEach(function(data) {
+  hauntedData.forEach(function (data) {
 
     lat = data.Latitude;
     lng = data.Longitude;
     hauntedMarkers.push(
-      L.marker([lat, lng]).bindPopup("<h1>" + data.Address + "</h1> <hr> <h3>" + data.Blurb + "</h3>"));
+      L.marker([lat, lng], { icon: ghostIcon }).bindPopup("<h3>" + data.Blurb + "</h3>"));
   });
 });
+
 console.log(hauntedMarkers);
 
-var customLayer = L.geoJson(null, {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(feature.properties.Title);
-  }
+var CPLMarkers = [];
+// d3.csv('CPLdata1.csv', function(data) {
+// console.log(data)
+// })
+d3.csv("CPLdata1.csv", function (data) {
+  console.log(data);
+
+  // Cast each hours value in tvData as a number using the unary + operator
+  data.forEach(function (d) {
+
+    lat = d.Latitude;
+    lng = d.Longitude;
+    CPLMarkers.push(
+      L.marker([lat, lng], { icon: CPLIcon }).bindPopup("<h3 align = 'center'>" + d.Name + "</h3><h4 align = 'center'>" + d.Hours + "<br></br>" + d.Phone + "<br></br>" + d.Address + "<br></br><a href=" + d.Website + " target = '_blank'>" + `Website` + "</a></h4>"));
+  });
 });
-var CPLdata = omnivore.csv('CPLdata1.csv',null,customLayer)
-  .on('read',function(){
-      for (var key in customLayer._layers){
-        customLayer._layers[key].addTo(CPLdata);
-      }
-  })
-console.log(CPLLayer)
+
+
+
+
+
 //Define createMap function
 function createMap(neighborhoods) {
 
@@ -276,7 +295,22 @@ function createMap(neighborhoods) {
   });
 
   var hauntedLayer = L.layerGroup(hauntedMarkers);
-  // var CPLLayer = L.layerGroup(CPL)
+  var CPLLayer = L.layerGroup(CPLMarkers);
+  // var CTALayer = L.gridLayer.googleMutant({
+  //   type: 'hybrid'
+  // })
+  var CTALayer = L.gridLayer.googleMutant({
+    type: 'terrain',
+    styles: [
+      
+      {featureType: 'transit.station'},
+      {featureType: 'transit.line'}
+    ]
+  
+  });
+  CTALayer.addGoogleLayer('TransitLayer');
+  
+  
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -288,14 +322,15 @@ function createMap(neighborhoods) {
   var overlayMaps = {
     Neighborhoods: neighborhoods,
     Ghosts: hauntedLayer,
-    Libraries: CPLLayer
+    Libraries: CPLLayer,
+    CTA: CTALayer
   };
 
   // Create our map, giving it the streetmap and neighborhood layers to display on load
   var myMap = L.map("map", {
     center: [41.8781, -87.6298],
     zoom: 11,
-    layers: [streetmap, hauntedLayer,CPLLayer]
+    layers: [streetmap, hauntedLayer, CPLLayer, CTALayer]
   });
 
   // Create a layer control
